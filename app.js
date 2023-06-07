3fd2250150d035c88b6042685f16ec059b9178af
saurabh sanap <sanapsaurabh218@gmail.com>
2023-06-07 10:56:52 +0530

    coming soon page

---
+++ app.js
@@ -0,0 +1,12 @@
+/* -----------------------------------------------
+/* How to use? : Check the GitHub README
+/* ----------------------------------------------- */
+
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', './particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
+
+
+/* Otherwise just put the config content (json): */
+
